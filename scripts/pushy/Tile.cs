7f6c950d55377d5b5e56c2c5c8ec8ec62a716e7f
git-bauerseb <48291831+git-bauerseb@users.noreply.github.com>
2021-08-13 03:14:25 +0200

    Raw version ready; Boxes can be moved

--- scripts/pushy/Tile.cs
+++ scripts/pushy/Tile.cs
@@ -1,5 +1,21 @@
+using EngineLearning.scripts.grid_game.misc;
+using Godot;
+
 namespace EngineLearning.scripts.pushy {
     public class Tile {
-        
+        internal Sprite Sprite { get; set; }
+        internal Vector2Int Coordinates { get; set; }
+        internal Vector2Int OldCoordinates { get; set; }
+
+        public Tile(Sprite sprite, Vector2Int coords) {
+            Sprite = sprite;
+            Coordinates = coords;
+        }
+
+        public void UpdatePosition(Vector2Int newCoords) {
+            var temp = Coordinates;
+            Coordinates = newCoords;
+            OldCoordinates = temp;
+        }
     }
 }
